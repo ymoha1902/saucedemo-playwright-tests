fbfa6b7e3e0226bebfd24a63f4508e5e4ff387e9
StudentMoe <moha0769@algonquinlive.com>
2026-07-02 15:19:17 -0400

    Add GitHub Actions CI workflow for Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig ({ testDir: './tests',
     use: {
         headless: false,
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
     },
 
 });
