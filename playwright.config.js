3059b6aaeb765dc2f9a0bc772e2744330c347291
StudentMoe <moha0769@algonquinlive.com>
2026-07-02 15:35:06 -0400

    Use headless mode in CI, headed locally via process.env.CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig ({ testDir: './tests',
     use: {
-        headless: true,
+        headless: process.env.CI ? true : false,
         screenshot: 'only-on-failure',
         trace: 'retain-on-failure',
     },
